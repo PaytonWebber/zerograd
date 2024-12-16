5f6f147659730deb837a7bc318a2677d5041ac52
Payton Webber <paytonwebber@gmail.com>
2024-12-15 21:55:36 -0800

    add basic tensor creation functions

--- tensor/src/lib.rs
+++ tensor/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct Tensor {
+    shape: Vec<usize>,
+    data: Vec<f32>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Tensor {
+    pub fn new(shape: &[usize], data: Vec<f32>) -> Result<Self, &'static str> {
+        let expected_size: usize = shape.iter().product();
+        if data.len() != expected_size {
+            return Err("Data length does not match shape");
+        }
+        Ok(Tensor {
+            shape: shape.to_vec(),
+            data,
+        })
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn zeros(shape: &[usize]) -> Self {
+        let num_elements: usize = shape.iter().product();
+        Tensor {
+            shape: shape.to_vec(),
+            data: vec![0.0; num_elements],
+        }
+    }
+
+    pub fn ones(shape: &[usize]) -> Self {
+        let num_elements: usize = shape.iter().product();
+        Tensor {
+            shape: shape.to_vec(),
+            data: vec![1.0; num_elements],
+        }
+    }
+
+    pub fn shape(&self) -> &Vec<usize> {
+        &self.shape
+    }
+
+    pub fn data(&self) -> &Vec<f32> {
+        &self.data
     }
 }
